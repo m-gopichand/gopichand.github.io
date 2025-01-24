97b90413cb591d1b8c7724c882e0c2c6f90adf66
Madala Gopichand <80762088+m-gopichand@users.noreply.github.com>
2025-01-25 00:36:38 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Data Analytics", "Machine Learning","Deep Learning", "frontend development"],
+    strings: ["Data Science", "Machine Learning","Deep Engineering"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
