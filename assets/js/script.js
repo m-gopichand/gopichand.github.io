9c527088df7417b77cd5e72bec2ef0ac6ae12004
Madala Gopichand <80762088+m-gopichand@users.noreply.github.com>
2025-01-25 16:48:31 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Priyanshu Kumar Saw";
+            document.title = "Portfolio | Gopichand";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
